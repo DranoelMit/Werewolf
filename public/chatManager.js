58e95b17dbbfe377484e02be53823dab34dcb537
DranoelMit <tim.matthew.leonard@gmail.com>
2017-12-30 14:18:50 -0500

    Add usernames into the chat and keep a list of all online users next to the chat

--- public/chatManager.js
+++ public/chatManager.js
@@ -1,16 +1,38 @@
 $(function(){
-          var socket = io.connect();
-          var $messageForm = $("#messageForm");
-          var $message = $("#message");
-          var $chat = $("#chat");
+     var socket = io.connect();
+     var $chatArea = $("#chatArea");
+     var $messageForm = $("#messageForm");
+     var $message = $("#message");
+     var $chat = $("#chat");
+     var $usernameForm = $("#usernameForm");
+     var $users = $("#users");
+     var $userInput = $("#userInput");
 
-          $messageForm.submit(function(e){
-                    e.preventDefault();
-                    socket.emit("send message", $message.val());
-                    $message.val("");
+     $usernameForm.submit(function(e){
+          e.preventDefault();
+          socket.emit("new user", $userInput.val(), function(data){
+               if(data){
+                    $usernameForm.hide();
+                    $chatArea.show();
+               }
           });
+          $userInput.val("");
+     });
 
-          socket.on("new message", function(data){
-               $chat.append('<div class="newMessage">'+data.msg+'</div>');
-          });
+     $messageForm.submit(function(e){
+          e.preventDefault();
+          socket.emit("send message", $message.val());
+          $message.val("");
+     });
+     socket.on("get users", function(data){
+          let html ="";
+          for(let i=0; i<data.length; i++){
+               html += '<li class="username-list-item">' +data[i]+'</li>';
+          }
+          $users.html(html);
+     });
+
+     socket.on("new message", function(data){
+          $chat.append('<div class="newMessage"><span class="username">'+data.user+': </span>'+data.msg+'</div>');
+     });
 });
--- server.js
+++ server.js
@@ -22,12 +22,25 @@ io.sockets.on("connection", function(socket){
           //disconnect
      socket.on("disconnect", function(data)
      {
+          users.splice(users.indexOf(socket.username), 1);
+          updateUsernames();
           connections.splice(connections.indexOf(socket), 1);
           console.log("Discconected: " +connections.length + " sockets connected");
      });
-
+//send message
      socket.on("send message", function(data){
-          io.sockets.emit("new message", {msg:data});
+          io.sockets.emit("new message", {msg:data, user: socket.username});
      });
+//new user
+     socket.on("new user", function(data, callback){
+          callback(true);
+          socket.username = data;
+          users.push(socket.username);
+          updateUsernames();
+     });
+
+     function updateUsernames(){
+          io.sockets.emit("get users", users);
+     }
 
 });
