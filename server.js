b696fd3e39d6f1c0494b75535a0476555d289338
DranoelMit <tim.matthew.leonard@gmail.com>
2017-12-30 21:34:37 -0500

    turn autocomplete for forms off

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 8080);
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-     res.sendFile(__dirname + "/public/main.html");
+     res.sendFile(__dirname + "/public/index.html");
 });
 
 
